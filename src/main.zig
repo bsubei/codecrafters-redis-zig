7d1db5d9265e8320712af3620265481a114a6fb2
bsubei <6508762+bsubei@users.noreply.github.com>
2024-07-21 20:33:13 -0400

    ZU2, spawn concurrent threads to handle each client

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,40 @@
 const std = @import("std");
 const net = std.net;
+const stdout = std.io.getStdOut().writer();
+
+fn handleClient(client_connection: net.Server.Connection) !void {
+    defer client_connection.stream.close();
+
+    var buf: [1024]u8 = undefined;
+    while (true) {
+        const num_read_bytes = client_connection.stream.read(&buf) catch break;
+        try stdout.print("Read {d} bytes: {s}\n", .{ num_read_bytes, buf[0..num_read_bytes] });
+        if (num_read_bytes == 0) {
+            break;
+        }
+        const response = "+PONG\r\n";
+        try client_connection.stream.writeAll(response);
+        try stdout.print("Done sending response: {s} to client {}\n", .{ response, client_connection.address.in });
+    }
+}
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
-
     const address = try net.Address.resolveIp("127.0.0.1", 6379);
 
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
+    try stdout.print("Started listening at address: {}\n", .{address.in});
 
-    const connection = try listener.accept();
-    try stdout.print("accepted new connection from client {}\n", .{connection.address.in});
-    defer connection.stream.close();
+    // Keep listening to new client connections, and once one comes in, set it up to be handled be a new thread and go back to listening.
+    while (true) {
+        // TODO we delegate the responsibility of closing the connection to the spawned thread, but technically we could have an error before that happens.
+        const connection = try listener.accept();
 
-    var buf: [1024]u8 = undefined;
-    while ((connection.stream.read(&buf) catch 0) > 0) {
-        try connection.stream.writeAll("+PONG\r\n");
-        try stdout.print("Done sending to client {}\n", .{connection.address.in});
+        try stdout.print("accepted new connection from client {}\n", .{connection.address.in.sa.port});
+        // TODO join on these threads for correctness.
+        const t = try std.Thread.spawn(.{}, handleClient, .{connection});
+        t.detach();
     }
 }
